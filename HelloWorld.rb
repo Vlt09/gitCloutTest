c74e5042fd7c768bc3b3c39bdcfc9cc5981ef9f8
Valentin Barrau <119218941+Vlt09@users.noreply.github.com>
2023-12-25 17:46:25 +0100

    Create HelloWorld.rb

---
+++ HelloWorld.rb
@@ -0,0 +1,9 @@
+# Ceci est un commentaire sur une seule ligne
+
+=begin
+Ceci est un commentaire
+qui s'étend sur plusieurs lignes
+=end
+
+# Vous pouvez également ajouter des commentaires après le code
+puts "Hello, World!" # Ceci est un commentaire sur la même ligne que le code
